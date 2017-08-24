9a65d26213f530913dd99aa39cda92f15354de61
brain <brain@42specifications.com>
2017-08-24 12:08:17 +0800

    runsh the scripts

--- getMainStructure.sh
+++ getMainStructure.sh
@@ -1,41 +1,54 @@
 #!/bin/bash
 
-MAIN=$(curl -s --cookie cookie.txt --cookie-jar cookie.txt --data 'j_username=admin&j_password=secret' --location http://10.254.0.33:8080/ajax/menuData.action | jq -r '[.[] | {type: .addClass, id: .id, title: .title, childs: [(.children[] | {type: .addClass, id: .id, title: .title, childs: [(.children[] | {class: .addClass, id: .id, title: .title})]})]}]')
+SCRIPT_LOG_PREFIX=[`basename "$0"`]
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $DIR/tools/agilefant-automation-tools.sh
+source $DIR/conf/agilefant-automation-tools.conf
+
+log 101
+log 10
+
+agilefant-automation-login
+
+#echo "PETER"
+
+MAIN=$(curl -s --cookie $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --cookie-jar $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --location $AGILEFANT_HOST:$AGILEFANT_PORT$AGILEFANT_PATH/ajax/menuData.action | jq -r '[.[] | {type: .addClass, id: .id, title: .title, childs: [(.children[] | {type: .addClass, id: .id, title: .title, childs: [(.children[] | {class: .addClass, id: .id, title: .title})]})]}]')
 
 #echo $MAIN
 
 PRODUCT_COUNT="$(echo $MAIN | jq '. | length')"
-echo "PRODUCT_COUNT is: $PRODUCT_COUNT"
+log "PRODUCT_COUNT is: $PRODUCT_COUNT" 1
 for i in $(seq 1 "$PRODUCT_COUNT"); 
 do
 	PRODUCT="$(echo $MAIN | jq -r --arg I $i '.[$I | tonumber -1]')"
 	PRODUCT_ID=$(echo $PRODUCT | jq '. | .id')
-	echo "Working on product with id: $PRODUCT_ID"
+	log "Working on product with id: $PRODUCT_ID" 1
 	PRODUCT_CHILD_COUNT="$(echo $PRODUCT | jq '. | .childs | length')"
-	echo "PRODUCT_CHILD_COUNT is $PRODUCT_CHILD_COUNT"
+	log "PRODUCT_CHILD_COUNT is $PRODUCT_CHILD_COUNT" 1
 	for j in $(seq 1 "$PRODUCT_CHILD_COUNT");
 	do
 		PROJECT_OR_ITERATION=$(echo $PRODUCT | jq -r --arg J $j '. | .childs[$J | tonumber -1] | .type')
 		BACKLOG_ID=$(echo $PRODUCT | jq -r --arg J $j '. | .childs[$J | tonumber -1] | .id')
 		if [ "$PROJECT_OR_ITERATION" == "PROJECT" ]; then
-			echo "Working on project with id: $BACKLOG_ID"
+			log "Working on project with id: $BACKLOG_ID" 1
 			PROJECT_ITERATION_COUNT=$(echo $PRODUCT | jq -r --arg J $j '. | .childs[$J | tonumber -1] | .childs | length')
-			echo "Number of project iterations:  $PROJECT_ITERATION_COUNT"
+			log "Number of project iterations:  $PROJECT_ITERATION_COUNT" 1
 			PROJECT=$(echo $PRODUCT | jq -r --arg J $j '. | .childs[$J | tonumber -1]')
 			for k in $(seq 1 "$PROJECT_ITERATION_COUNT"); 
 			do
 				ITERATION=$(echo $PRODUCT | jq -r --arg J $j --arg K $k '. | .childs[$J | tonumber -1] | .childs[$K | tonumber -1]') 
 			done	
-			PROJECT_STORIES=$(curl -s --cookie cookie.txt --cookie-jar cookie.txt --data "projectId=$BACKLOG_ID" --location http://10.254.0.33:8080/ajax/getProjectStoryTree.action | grep -o -E "storyid\S+" | grep -o "[0-9]*")
+			PROJECT_STORIES=$(curl -s --cookie $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --cookie-jar $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --data "projectId=$BACKLOG_ID" --location $AGILEFANT_HOST:$AGILEFANT_PORT$AGILEFANT_PATH/ajax/getProjectStoryTree.action | grep -o -E "storyid\S+" | grep -o "[0-9]*")
 		
 			while read -ra STORIES; do
       			for l in "${STORIES[@]}"; do
-          			echo "Working on story with id: $l"
-					STORY_TASKS=$(curl -s --cookie cookie.txt --cookie-jar cookie.txt --data "projectId=$BACKLOG_ID" --location http://10.254.0.33:8080/ajax/retrieveStory.action?storyId=$l)
+          			log "Working on story with id: $l" 1
+					STORY_TASKS=$(curl -s --cookie $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --cookie-jar $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --data "projectId=$BACKLOG_ID" --location $AGILEFANT_HOST:$AGILEFANT_PORT$AGILEFANT_PATH/ajax/retrieveStory.action?storyId=$l)
 				TASK_IDS=$(echo $STORY_TASKS | jq -r '. | .tasks[] | .id')
 				while read -ra TASKS; do
 					for m in "${TASKS[@]}"; do
-						echo "Working on task with id: $m"
+						log "Working on task with id: $m" 1
 					done
 				done <<< "$TASK_IDS"	
       			done
@@ -43,7 +56,13 @@ do
 		fi
 		if [ "$PROJECT_OR_ITERATION" == "ITERATION" ]; then
 			ITERATION=$(echo $PRODUCT | jq -r --arg J $j '. | .childs[$J | tonumber -1]')
-			echo "Working on iteration with id: $BACKLOG_ID"
+			log "Working on iteration with id: $BACKLOG_ID" 1
 		fi
 	done
 done
+
+agilefant-automation-logout
+
+log 11
+log 101
+exit 0
--- tools/agilefant-automation-tools.sh
+++ tools/agilefant-automation-tools.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 agilefant-automation-login() {
+	log "Logging into agilefant" 1
 	if [ ! -d "$COOKIE_FILE_DIR" ]; then
-		echo "COOKIE_FILE_DIR does not exist. Creating it."
+		log "COOKIE_FILE_DIR $COOKIE_FILE_DIR does not exist. Creating it." 1
 		mkdir $COOKIE_FILE_DIR
 	fi
 	local CURL_OUTPUT
 	CURL_OUTPUT=$(curl -ivLs --cookie "$COOKIE_FILE_DIR/$COOKIE_FILE_NAME" --cookie-jar "$COOKIE_FILE_DIR/$COOKIE_FILE_NAME" --data "j_username=$AGILEFANT_USER&j_password=$AGILEFANT_PASSWD" --location "${AGILEFANT_HOST}:${AGILEFANT_PORT}${AGILEFANT_PATH}/j_spring_security_check" 2>&1)
+	log "CURL_OUTPUT is: $CURL_OUTPUT" 0
+	log "Logged into agilefant" 1
 }
 
 agilefant-automation-logout() {
+	log "Logging out of agilefant" 1
     if [ ! -d "$COOKIE_FILE_DIR" ]; then
-         echo "COOKIE_FILE_DIR does not exist. Cannot logout."
+         log "COOKIE_FILE_DIR does not exist. Cannot logout." 1
          return 1     
 	fi	
 	local CURL_OUTPUT
      CURL_OUTPUT=$(curl -ivLs --cookie "$COOKIE_FILE_DIR/$COOKIE_FILE_NAME" --cookie-jar "$COOKIE_FILE_DIR/$COOKIE_FILE_NAME" --data "j_username=$AGILEFANT_USER&j_password=$AGILEFANT_PASSWD" --location "${AGILEFANT_HOST}:${AGILEFANT_PORT}${AGILEFANT_PATH}/j_spring_security_logout?exit=Logout" 2>&1)
+	log "CURL_OUTPUT is: $CURL_OUTPUT" 0
 	rm -r $COOKIE_FILE_DIR
+	log "Logged out of agilefant and cleanup COOKIE_FILE_DIR" 1
 }
 
 export -f agilefant-automation-login
