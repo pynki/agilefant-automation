3131ef5e6672a70d621f4881630c6cf056d5c7b2
brain <brain@42specifications.com>
2017-08-25 10:28:24 +0800

    get everything in structure script, getter for objects in tools

--- getMainStructure.sh
+++ getMainStructure.sh
@@ -10,7 +10,6 @@ log 101
 log 10
 
 agilefant-automation-login
-MENU_DATA=""
 agilefant-automation-getMenuData MENU_DATA
 MAIN=$(echo $MENU_DATA | jq -r '[.[] | {type: .addClass, id: .id, title: .title, childs: [(.children[] | {type: .addClass, id: .id, title: .title, childs: [(.children[] | {class: .addClass, id: .id, title: .title})]})]}]')
 PRODUCT_COUNT="$(echo $MAIN | jq '. | length')"
@@ -34,27 +33,55 @@ do
 			for k in $(seq 1 "$PROJECT_ITERATION_COUNT"); 
 			do
 				ITERATION=$(echo $PRODUCT | jq -r --arg J $j --arg K $k '. | .childs[$J | tonumber -1] | .childs[$K | tonumber -1]') 
+				ITERATION_ID=$(echo $ITERATION | jq '. | .id')
+				agilefant-automation-getIteration ITERATION_JSON  $ITERATION_ID
+				ITERATION_TASKS_COUNT=$(echo $ITERATION_JSON | jq '. | .tasks | length')
+#				echo "IT TASKS COUNT: $ITERATION_TASKS_COUNT"
+				
+				for x in $(seq 1 "$ITERATION_TASKS_COUNT");
+				do
+					TASK_ID=$(echo $ITERATION_JSON | jq -r --arg X $x '. | .tasks[$X | tonumber -1]  | .id')
+					log "Working on task with ID: $TASK_ID" 1
+				done
 			done
-			PROJECT_STORY_TREE=""
 			agilefant-automation-getProjectStoryTree PROJECT_STORY_TREE $BACKLOG_ID	
 			PROJECT_STORIES=$(echo $PROJECT_STORY_TREE | grep -o -E "storyid\S+" | grep -o "[0-9]*")
 			while read -ra STORIES; do
       			for l in "${STORIES[@]}"; do
           			log "Working on story with id: $l" 1
-					STORY_TASKS=""
-					agilefant-automation-getStory STORY_TASKS $BACKLOG_ID $l
-				TASK_IDS=$(echo $STORY_TASKS | jq -r '. | .tasks[] | .id')
-				while read -ra TASKS; do
-					for m in "${TASKS[@]}"; do
-						log "Working on task with id: $m" 1
-					done
-				done <<< "$TASK_IDS"	
+					agilefant-automation-getStory STORY_TASKS $l
+
+#echo "ccccccccccccccccc $STORY_TASKS"
+
+				STORY_TASKS_COUNT=$(echo $STORY_TASKS | jq -r '. | .tasks | length')
+#echo "VVVVVVVV: $STORY_TASKSS"
+#echo "ZZZZZZZZZZZZ: $TASK_IDS"
+				for z in $(seq 1 "$STORY_TASKS_COUNT");
+				do
+					TASK_ID=$(echo $STORY_TASKS | jq --arg Z $z '. | .tasks[$Z | tonumber -1] | .id')
+				log "Working on task: $TASK_ID" 1
+				done
+#				while read -ra TASKS; do
+#					for m in "${TASKS[@]}"; do
+#						log "Working on task with id: $m" 1
+#					done
+#				done <<< "$TASK_IDS"	
       			done
  			done <<< "$PROJECT_STORIES"
 		fi
 		if [ "$PROJECT_OR_ITERATION" == "ITERATION" ]; then
 			ITERATION=$(echo $PRODUCT | jq -r --arg J $j '. | .childs[$J | tonumber -1]')
 			log "Working on iteration with id: $BACKLOG_ID" 1
+			ITERATION_ID=$(echo $ITERATION | jq '. | .id')
+			agilefant-automation-getIteration ITERATION_JSON  $ITERATION_ID
+            ITERATION_TASKS_COUNT=$(echo $ITERATION_JSON | jq '. | .tasks | length')
+ #           echo "IT TASKS COUNT: $ITERATION_TASKS_COUNT"
+            for x in $(seq 1 "$ITERATION_TASKS_COUNT");
+            do
+                TASK_ID=$(echo $ITERATION_JSON | jq -r --arg X $x '. | .tasks[$X | tonumber -1]  | .id')
+                log "Working on task with ID: $TASK_ID" 1
+            done
+            
 		fi
 	done
 done
@@ -63,3 +90,11 @@ agilefant-automation-logout
 log 11
 log 101
 exit 0
+
+# json should look like this:
+# {products: [{product1}, {product2}, ...], user: [{user1}, {user2}, ...]}
+# a product looks like this: {id:1,data: {}, standalone: true/false, projects: [{id:1,data:{}, iterations: [{id:1,data:{}, tasks:[{id:1,data:{}}]}], stories: [{id:1,data:{}, tasks:[{id:1,data:{}}]}]}]}
+#a user looks like this: {id:1,data:{}}
+
+
+
--- tools/agilefant-automation-tools.sh
+++ tools/agilefant-automation-tools.sh
@@ -25,10 +25,10 @@ agilefant-automation-logout() {
 	local CURL_OUTPUT
      CURL_OUTPUT=$(curl -ivLs --cookie "$COOKIE_FILE_DIR/$COOKIE_FILE_NAME" --cookie-jar "$COOKIE_FILE_DIR/$COOKIE_FILE_NAME" --data "j_username=$AGILEFANT_USER&j_password=$AGILEFANT_PASSWD" --location "${AGILEFANT_HOST}:${AGILEFANT_PORT}${AGILEFANT_PATH}/j_spring_security_logout?exit=Logout" 2>&1)
 	log "CURL_OUTPUT is: $CURL_OUTPUT" 0
-	log "Logging out of agilefant" 1
+	log "Logged out of agilefant" 1
 	if [ $CLEANUP_TMP_DIR == 1 ]; then
 		rm -r $COOKIE_FILE_DIR
-		log "Cleaned up COOKIE_FILE_DIR" 1
+		log "Cleaned up COOKIE_FILE_DIR - well, just removed it..." 1
 	fi
 #TODO check if we are logged out
 }
@@ -52,17 +52,86 @@ agilefant-automation-getProjectStoryTree() {
 	log "CURL_OUTPUT is: $CURL_OUTPUT" 0
 }
 
-# call agilefant-automation-getStory $RETURN_VAL $PROJECT_BACKLOG_ID $STORY_ID
+# call agilefant-automation-getStory $RETURN_VAL $STORY_ID
 agilefant-automation-getStory() {
-	log "Getting story with id $3 from project with backlogId: $2" 1
+	log "Getting story with id $2" 1
 	declare -n reVal=$1
-	local CURL_OUTPUT=$(curl -s --cookie $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --cookie-jar $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --data "projectId=$2" --location $AGILEFANT_HOST:$AGILEFANT_PORT$AGILEFANT_PATH/ajax/retrieveStory.action?storyId=$3)
+	local CURL_OUTPUT=$(curl -s --cookie $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --cookie-jar $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --location $AGILEFANT_HOST:$AGILEFANT_PORT$AGILEFANT_PATH/ajax/retrieveStory.action?storyId=$2)
 	reVal=$CURL_OUTPUT
 	log "CURL_OUTPUT is: $CURL_OUTPUT" 0
 }
 
+# call agilefant-automation-getProduct $RETURN_VAL $PRODUCT_BACKLOG_ID
+agilefant-automation-getProduct() {
+    log "Getting product with backlogId $2" 1
+    declare -n reVal=$1
+    local CURL_OUTPUT=$(curl -s --cookie $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --cookie-jar $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --location $AGILEFANT_HOST:$AGILEFANT_PORT$AGILEFANT_PATH/ajax/retrieveProduct.action?productId=$2)
+    reVal=$CURL_OUTPUT
+    log "CURL_OUTPUT is: $CURL_OUTPUT" 0
+}
+
+# call agilefant-automation-getProject $RETURN_VAL $PROJECT_BACKLOG_ID
+agilefant-automation-getProject() {
+    log "Getting project with backlogId $2" 1
+    declare -n reVal=$1
+    local CURL_OUTPUT=$(curl -s --cookie $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --cookie-jar $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --location $AGILEFANT_HOST:$AGILEFANT_PORT$AGILEFANT_PATH/ajax/projectData.action?projectId=$2)
+    reVal=$CURL_OUTPUT
+    log "CURL_OUTPUT is: $CURL_OUTPUT" 0
+}
+
+# call agilefant-automation-getProjectTotalSpentEffort $RETURN_VAL $PROJECT_BACKLOG_ID
+agilefant-automation-getProjectTotalSpentEffort() {
+    log "Getting total spend effort of project with backlogId $2" 1
+    declare -n reVal=$1
+    local CURL_OUTPUT=$(curl -s --cookie $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --cookie-jar $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --location $AGILEFANT_HOST:$AGILEFANT_PORT$AGILEFANT_PATH/ajax/projectTotalSpentEffort.action?projectId=$2)
+    reVal=$CURL_OUTPUT
+    log "CURL_OUTPUT is: $CURL_OUTPUT" 0
+}
+
+# this gives us the tasks that have no story and belong to an iteration
+# call agilefant-automation-getIteration $RETURN_VAL $ITERATION_BACKLOG_ID
+agilefant-automation-getIteration() {
+    log "Getting iteration with backlogId $2" 1
+    declare -n reVal=$1
+    local CURL_OUTPUT=$(curl -s --cookie $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --cookie-jar $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --location $AGILEFANT_HOST:$AGILEFANT_PORT$AGILEFANT_PATH/ajax/iterationData.action?iterationId=$2)
+    reVal=$CURL_OUTPUT
+    log "CURL_OUTPUT is: $CURL_OUTPUT" 0
+}
+
+# this is a dirty hack - there is no getTaskData call as far as i could see
+# call agilefant-automation-getTask $RETURN_VAL $TASK_ID
+agilefant-automation-getTask() {
+    log "Getting task with taskId $2" 1
+    declare -n reVal=$1
+    local CURL_OUTPUT=$(curl -s --cookie $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --cookie-jar $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --data "taskId=$2" --location $AGILEFANT_HOST:$AGILEFANT_PORT$AGILEFANT_PATH/ajax/storeTask.action)
+    reVal=$CURL_OUTPUT
+    log "CURL_OUTPUT is: $CURL_OUTPUT" 0
+}
+
+# call agilefant-automation-getUsers $RETURN_VAL
+agilefant-automation-getUsers() {
+    log "Getting all users" 1
+    declare -n reVal=$1
+    local CURL_OUTPUT=$(curl -s --cookie $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --cookie-jar $COOKIE_FILE_DIR/$COOKIE_FILE_NAME --location $AGILEFANT_HOST:$AGILEFANT_PORT$AGILEFANT_PATH/ajax/retrieveAllUsers.action)
+    reVal=$CURL_OUTPUT
+    log "CURL_OUTPUT is: $CURL_OUTPUT" 0
+}
+
+#TODO BIG ONE: build helper function to read out the data of objects from the JSON build in mainstructure script. get main struct reads into a var, that var is passed and then the data returned. the id of the data wanted is passed to. like this: getProjectDataFromTree($project_id $tree)
+
+#TODO get all objects of a type with their data, return json
+#TODO get all objects of a type that meet specific requirements, return json.this is a massive filtering system. need to think carefully about what filters are needed. genereic filters with jq?  
+
+#TODO all the setter for the objects!
+#TODO all the creators for the objects!
 export -f agilefant-automation-login
 export -f agilefant-automation-logout
 export -f agilefant-automation-getMenuData
 export -f agilefant-automation-getProjectStoryTree
 export -f agilefant-automation-getStory
+export -f agilefant-automation-getProduct
+export -f agilefant-automation-getProject
+export -f agilefant-automation-getProjectTotalSpentEffort
+export -f agilefant-automation-getIteration
+export -f agilefant-automation-getTask
+export -f agilefant-automation-getUsers
